ab433e8aeaef6694b3c6ab623830bc9a2fac786b
TheGreaMarat <marat.ttl79@gmail.com>
2019-11-06 23:35:14 +0300

    add class init and query methods

--- rashid/simple_nn.py
+++ rashid/simple_nn.py
@@ -2,14 +2,29 @@
 Простая нейронная сеть из книги Тарика Рашидаю.
 """
 
+import numpy
+import scipy.special
+
 
 class NeuralNetwork:
 
-    def __init__(self):
+    def __init__(self, input_nodes: int, hidden_nodes: int, output_nodes: int, learnig_rate: float):
         """
         Init the params.
         """
-        pass
+
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+
+        self.learning_rate = learnig_rate
+
+        # init weights of layers
+        self.weights_inner_to_hidden = (numpy.random.rand(self.hidden_nodes, self.input_nodes) - 0.5)
+        self.weights_hidden_to_output = (numpy.random.rand(self.output_nodes, self.hidden_nodes) - 0.5)
+
+        # set activation functoin
+        self.activation_function = lambda x: scipy.special.expit(x)
 
     def train(self):
         """
@@ -19,11 +34,20 @@ class NeuralNetwork:
         """
         pass
 
-    def query(self):
+    def query(self, inputs_list):
         """
         Show network params.
 
         :return:
         """
-        pass
+
+        inputs = numpy.array(inputs_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.weights_inner_to_hidden, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.weights_hidden_to_output, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
 
