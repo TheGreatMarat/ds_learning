258cdc2f7c55a556c177436732ba6246cbd217ca
TheGreaMarat <marat.ttl79@gmail.com>
2019-11-06 23:05:43 +0300

    start to wotk with Taric Rashid book, add skeleton of NeuralNetwork class.

---
+++ rashid/simple_nn.py
@@ -0,0 +1,29 @@
+"""
+Простая нейронная сеть из книги Тарика Рашидаю.
+"""
+
+
+class NeuralNetwork:
+
+    def __init__(self):
+        """
+        Init the params.
+        """
+        pass
+
+    def train(self):
+        """
+        Train network.
+
+        :return:
+        """
+        pass
+
+    def query(self):
+        """
+        Show network params.
+
+        :return:
+        """
+        pass
+
